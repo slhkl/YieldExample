db623ad425be95bd835db840f69bc2ca6c1e18ff
Salih Kol <salih.kol@detaysoft.com>
2023-08-28 15:22:09 +0300

    Add project files.

---
+++ App/Cars.cs
@@ -0,0 +1,40 @@
+﻿namespace App
+{
+    public class Cars
+    {
+        private readonly static IEnumerable<string> enumerableCarList = new List<string>
+        {
+            "McLaren",
+            "Mercedes",
+            "Ferrari",
+            "Aston Martin",
+            "Audi",
+            "Bmw",
+            "Togg",
+            "Tofas",
+            "Fiat",
+            "Volvo"
+        };
+
+        public static IEnumerable<string> GetCars()
+        {
+            return enumerableCarList;
+        }
+
+        public static IEnumerable<string> GetCarsFromList()
+        {
+            List<string> carList = new List<string>();
+
+            foreach (string car in enumerableCarList)
+                carList.Add(car);
+            return carList;
+        }
+
+        public static IEnumerable<string> GetCarsFromYieldReturn()
+        {
+            foreach (string car in enumerableCarList)
+                yield return car;
+        }
+
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,25 @@
+﻿using App;
+using System.Diagnostics;
+
+IEnumerable<string> cars = null;
+
+Stopwatch sw = Stopwatch.StartNew();
+cars = Cars.GetCars();
+foreach (var car in cars)
+    Console.Write(car + "\n");
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
+
+sw.Restart();
+cars = Cars.GetCarsFromList();
+foreach (var car in cars)
+    Console.Write(car + "\n");
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
+
+sw.Restart();
+cars = Cars.GetCarsFromYieldReturn();
+foreach (var car in cars)
+    Console.Write(car + "\n");
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
